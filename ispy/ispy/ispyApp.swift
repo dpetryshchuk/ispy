a9b0506d3d45b7b18785254bae9237f8011cd8fc
Dmytro Petryshchuk <dmytrodim@gmail.com>
2026-04-20 14:20:11 -0700

    chore: remove leftover Flutter artifacts (.dart_tool, .idea, ios remnant)

---
+++ ispy/ispy/ispyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ispyApp.swift
+//  ispy
+//
+//  Created by Dmytro Petryshchuk on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct ispyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
